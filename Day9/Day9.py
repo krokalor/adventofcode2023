ea6cf476bfffa56b91ee1d51d3e3736c2b05212b
karol-kulinowski <karol.kulinowski@gmail.com>
2023-12-27 14:47:53 +0100

    Part 1 of day 9

--- Day9/Day9.py
+++ Day9/Day9.py
@@ -14,7 +14,8 @@ class Day9:
     @staticmethod
     def part1(test=True):
         """9.1"""
-        print('AoC23: 8.1\n')
+        print('AoC23: 9.1')
+        print('----------')
         Day9.__input = 'test.txt' if test else Day9.__input
         with open(Day9.__input, 'r') as file:
             lines = file.readlines()
@@ -22,7 +23,27 @@ class Day9:
         for line in lines:
             history.append([int(i) for i in line.split()])
         print(history)
+        print()
+        ex_sum = 0
+        for h in history:
+            diff = [h]
+            while True:
+                # print(h)
+                if sum(h) == 0:
+                    break
+                else:
+                    h = list(np.diff(h))
+                    diff.append(h)
+            diff[-1].append(0)
+            print(diff[-1])
+            for i in range(len(diff)-1)[::-1]:
+                diff[i].append(diff[i][-1]+diff[i+1][-1])
+                print(diff[i])
+            ex = diff[0][-1]
+            ex_sum += ex
+            print(ex, ex_sum)
+            print()
 
 
 if __name__ == '__main__':
-    Day9.part1()
+    Day9.part1(False)
