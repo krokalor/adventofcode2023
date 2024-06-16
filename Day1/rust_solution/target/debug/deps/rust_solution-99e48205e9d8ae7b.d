23826e399c691448bf8d38be3eeb02ada43c5bd2
karlie-ui <karol.kulinowski@gmail.com>
2024-06-16 13:30:32 +0200

    Move main.rs to 'rust_solution'

---
+++ Day1/rust_solution/target/debug/deps/rust_solution-99e48205e9d8ae7b.d
@@ -0,0 +1,5 @@
+/home/karolk/Documents/adventofcode2023/Day1/rust_solution/target/debug/deps/rust_solution-99e48205e9d8ae7b: src/main.rs
+
+/home/karolk/Documents/adventofcode2023/Day1/rust_solution/target/debug/deps/rust_solution-99e48205e9d8ae7b.d: src/main.rs
+
+src/main.rs:
---
+++ Day1/rust_solution/target/debug/rust_solution.d
@@ -0,0 +1,1 @@
+/home/karolk/Documents/adventofcode2023/Day1/rust_solution/target/debug/rust_solution: /home/karolk/Documents/adventofcode2023/Day1/rust_solution/src/main.rs
